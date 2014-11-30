da3deb90564c3f5d8d0ca06344a3514664daac33
Judson <nyarly@gmail.com>
2014-11-29 16:46:59 -0800

    Hooks needed more punching

--- lib/rspec-steps/duckpunch/hooks.rb
+++ lib/rspec-steps/duckpunch/hooks.rb
@@ -3,5 +3,13 @@ require 'rspec/core/hooks'
 module RSpec::Core::Hooks
   class HookCollections
     SCOPES << :step
+
+    def initialize(owner, data)
+      @owner = owner
+      @data = data.merge(
+        :before => data[:before].merge(:step => HookCollection.new),
+        :after  => data[:after ].merge(:step => HookCollection.new)
+      )
+    end
   end
 end
